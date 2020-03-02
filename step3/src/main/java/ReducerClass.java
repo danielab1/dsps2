8760899bfb5c9a72866e8519e7b7d6bacf3974ac
danielab1 <danielab@post.bgu.ac.il>
2020-03-02 11:48:43 +0200

    and change the probility

--- step3/src/main/java/ReducerClass.java
+++ step3/src/main/java/ReducerClass.java
@@ -12,6 +12,7 @@ public class ReducerClass extends Reducer<TripleKey, TripleValue, Text, DoubleWr
     private double C0;
     private double C1;
     private double C2;
+
     // in this round we get N2 from the sort and we can calc the final probability
     @Override
     protected void reduce(TripleKey key, Iterable<TripleValue> values, Context context) throws IOException, InterruptedException {
@@ -26,21 +27,17 @@ public class ReducerClass extends Reducer<TripleKey, TripleValue, Text, DoubleWr
             if (key.getThirdWord().equals("~"))
                 N2 = value.N2.get();
         }
-        double probability = -1;
+        double probability;
 
         if (!key.getThirdWord().equals("~")) {
 
             double K2 = (Math.log10(N2 + 1) + 1) / (Math.log10(N2 + 1) + 2);
             double K3 = (Math.log10(N3 + 1) + 1) / (Math.log10(N3 + 1) + 2);
-            try{
-                probability = K3 * (N3 /C2) + (1-K3) * K2*(N2/C1) + (1-K3)* (1-K2) * (N1/C0);
+            probability = K3 * (N3 / C2) + (1 - K3) * K2 * (N2 / C1) + (1 - K3) * (1 - K2) * (N1 / C0);
 
-                    }
-                    catch (MathArithmeticException e){
-                        System.out.println("divided in zero");
-                    }
-
-                context.write(new Text(key.toString()), new DoubleWritable(probability));
+            if (Double.isInfinite(probability))
+                probability = 1;
+            context.write(new Text(key.toString()), new DoubleWritable(probability));
 
         }
     }
